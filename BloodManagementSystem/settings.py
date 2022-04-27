86570f95bbeababb6187dfa953d6813f1c5eb3a1
Alok Yadav <40895847+Alok8@users.noreply.github.com>
2022-04-27 14:23:41 +0530

    Update settings.py
    Some changes have been done.

--- BloodManagementSystem/settings.py
+++ BloodManagementSystem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v!841yxs3b%q@_2)m9+nt)tet)eh3um6m17!gagqyc=8+zf6c5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
